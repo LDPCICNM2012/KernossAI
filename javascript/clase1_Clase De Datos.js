92671ae0318c17e99f99af6a4f30d9f1bc7137cc
LDPCICNM2012 <depabloscuevaslander@gmail.com>
2026-07-25 23:37:32 +0200

    Aprendiendo JavaScript

---
+++ javascript/clase1_Clase De Datos.js
@@ -0,0 +1,63 @@
+2 + 2 // 4
+// Los // son comeentarios como # en python.
+
+//Estan estas operaciones:
+// + SUMA
+// - RESTA
+// * MULTIPLICACION
+// / DIVISION
+// % MODULO
+// ** EXPONENTE
+
+'Hola me llamo Lander' // Las '' son para poner cadenas de texto llamadas strings.
+
+;`Hola me llamo Lander
+Voy a 2º de la eso y estoy aprendiendo javascript
+Me gusta programar y hacer apps` // Las ``son para poner cadenas de texto en mas de una liena.
+
+'Hola me llamo Lander' + ' y estoy aprendiendo javascript' // Tambien se puede usar + para conectar cadenas de texto.
+
+True
+False // Son booleanos igual que en python.
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+//OPERADORES DE COMPARACION
+
+ 5 > 3 // True
+ 3 > 5 // False
+ 5 >= 5 // True
+ 5 <= 3 // False
+ // BASICAMENTE COMO EN PYTHON, >, <, >=, <=, ===, !==
+ // TAMBIEN SE PUEDE UTILIZAR PARA CADENAS DE TEXTO COMO:
+ 'JAVA' === 'JAVA' // True
+ 'JAVA' === 'java' // False
+ 'JAVA' !== 'java' // True
+ 'JAVA' !== 'JAVA' // False
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+//OPERADORES LOGICOS
+
+// AND es &&
+// OR es ||
+// NOT es !
+
+// EJEMPLOS DE AND:
+
+true && true // True
+true && false // False
+false && true // False
+
+// EJEMPLOS DE OR:
+
+true || true // True
+true || false // True
+false || false // False
+
+// EJEMPLOS DE NOT:
+
+!true // False
+!false // True
+
+
